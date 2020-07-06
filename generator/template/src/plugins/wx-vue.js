bb4efd596234f0b4369cce4cf64b5974cd60188e
alex8088 <244096523@qq.com>
2020-07-06 16:51:42 +0800

    fix: theme import

--- generator/template/src/plugins/wx-vue.js
+++ generator/template/src/plugins/wx-vue.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
-<%_ if (options.customTheme) { _%>
-import '../styles/wx-theme.css'
-<%_ } _%>
 <%_ if (options.import === 'full') { _%>
 import 'wx-vue/lib/styles/index.css'
 import Wx from 'wx-vue'
+<%_ if (options.customTheme) { _%>
+import '../styles/wx-theme.css'
+<%_ } _%>
 
 Vue.use(Wx)
 <%_ } else { _%>
 import { Button } from 'wx-vue'
+<%_ if (options.customTheme) { _%>
+import '../styles/wx-theme.css'
+<%_ } _%>
 
 Vue.use(Button)
 <%_ } _%>
